c7b3f0a56d4f324e28865c58120b361ab0149f20
CosmicOppai <cosmicoppai@protonmail.com>
2024-10-19 23:46:33 +0530

    fixed DATABASE_DSN

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,13 +16,13 @@ if [ ! -f ./monaco-latest.osm.pbf ]; then
     wget -O secondary_importance.sql.gz https://nominatim.org/data/wikimedia-secondary-importance.sql.gz
     wget https://nominatim.org/data/gb_postcodes.csv.gz
     wget https://nominatim.org/data/us_postcodes.csv.gz
-fi
 
-echo "Starting Nominatim import..."
-NOMINATIM_DATABASE_DSN="pgsql:host=pg;dbname=nominatim;user=${USERNAME};password=${POSTGRES_PASSWORD}" nominatim import \
+    echo "Starting Nominatim import..."
+    NOMINATIM_DATABASE_DSN="pgsql:host=pg;dbname=nominatim;user=${USERNAME};password=${POSTGRES_PASSWORD}" nominatim import \
     --osm-file ./monaco-latest.osm.pbf \
     --project-dir . \
     2>&1 | tee setup.log
+fi
 
 
 exec gunicorn -b 0.0.0.0:8080 -w 4 -k uvicorn.workers.UvicornWorker nominatim_api.server.falcon.server:run_wsgi
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl "localhost:80/reverse?format=json&lat=43.7384&lon=7.4246"
+curl "localhost:80/status"
