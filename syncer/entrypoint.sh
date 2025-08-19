4e46eaac1ec51bf92f4ed6695157a85261c894bc
CosmicOppai <cosmicoppai@protonmail.com>
2025-08-19 16:57:26 +0530

    refactoring

--- syncer/entrypoint.sh
+++ syncer/entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 nominatim replication --init
 
 while true; do
-  sleep 86400
   echo "Staring Sync"
   nominatim replication --catch-up --threads 10
   nominatim refresh --postcodes
   echo "Sync Done"
+  sleep 86400
 done
